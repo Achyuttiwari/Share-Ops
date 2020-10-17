2b0c9fbed25165c2f76784ab120e19033066f7ed
Mokey-D-luffy88 <atching123@gmail.com>
2020-10-17 22:43:09 +0530

    overriding onActivityResult method for activity result

--- app/src/main/java/com/example/share_ops/FileChooser.java
+++ app/src/main/java/com/example/share_ops/FileChooser.java
@@ -1,5 +1,6 @@
 package com.example.share_ops;
 
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -18,14 +19,21 @@ public class FileChooser extends AppCompatActivity {
         filePath = (TextView) findViewById(R.id.text_filePath);
         permissionFile();
     }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == PERMISSION_REQUEST_CODE && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
             {
-                Toast.makeText(this,"permission already granted",Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "permission already granted", Toast.LENGTH_SHORT).show();
 
             }
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
     }
 }
