f28b56e7eaa4b437cef9931ad5947af19f6f25e1
“arpan-khandelwal” <akpalwal@gmail.com>
2020-10-18 00:12:08 +0530

    all the packages required are added

---
+++ app/src/main/java/com/example/share_ops/MainActivity_Sender.java
@@ -0,0 +1,24 @@
+package com.example.wifishareit;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.format.Formatter;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
