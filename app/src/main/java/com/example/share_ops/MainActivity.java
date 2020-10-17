37272ef7b48fb4d9be4c83a6339ac78f24d921ae
Mokey-D-luffy88 <atching123@gmail.com>
2020-10-17 21:16:47 +0530

    comparing requestCode with PERMISSION_REQUEST_INTERNET variable which is 0 initially

--- app/src/main/java/com/example/share_ops/MainActivity.java
+++ app/src/main/java/com/example/share_ops/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.share_ops;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 
@@ -24,5 +25,11 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
 
+        if(requestCode == PERMISSION_REQUEST_INTERNET) {
+            if(grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            }
+
+        }
     }
 }
