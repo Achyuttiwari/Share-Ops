8a8800c9059b20d2fb2dd2d44d07b0b7c6a1b6de
Mokey-D-luffy88 <atching123@gmail.com>
2020-10-17 22:38:00 +0530

    creating view for file_choosing activity

--- app/src/main/java/com/example/share_ops/FileChooser.java
+++ app/src/main/java/com/example/share_ops/FileChooser.java
@@ -1,9 +1,19 @@
 package com.example.share_ops;
 
+import android.os.Bundle;
 import android.widget.TextView;
 
-public class FileChooser {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FileChooser extends AppCompatActivity {
     TextView filePath;
     int PERMISSION_REQUEST_CODE = 1;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_file_chooser);
+        filePath = (TextView) findViewById(R.id.text_filePath);
+        permissionFile();
+    }
 }
