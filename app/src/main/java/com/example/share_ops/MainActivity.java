a6514b93e3c96ba5636841a88b21e8d2b8ee2d8c
Mokey-D-luffy88 <atching123@gmail.com>
2020-10-17 20:48:27 +0530

    delete unnecessary files

--- app/src/main/java/com/example/share_ops/MainActivity.java
+++ app/src/main/java/com/example/share_ops/MainActivity.java
@@ -1,24 +1,15 @@
 package com.example.share_ops;
 
 import android.os.Bundle;
-import android.view.View;
-import android.view.Menu;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration mAppBarConfiguration;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
