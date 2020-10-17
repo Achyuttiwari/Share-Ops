e6e46c3b4ac217ec0e832906ee9433eeb6733259
Mokey-D-luffy88 <atching123@gmail.com>
2020-10-17 22:21:05 +0530

    method for asking internet permission

--- app/src/main/java/com/example/share_ops/MainActivity.java
+++ app/src/main/java/com/example/share_ops/MainActivity.java
@@ -48,6 +48,9 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat {
             requestInternetPermission();
         }
     }
+    private void requestInternetPermission() {
+        {
 
+        }
 }
 }
