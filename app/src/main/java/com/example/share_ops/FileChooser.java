52f01ce65036eb90a88646fdd1f36abfd3dbf7c1
Mokey-D-luffy88 <atching123@gmail.com>
2020-11-25 15:27:10 +0530

    show all the file on media show

--- app/src/main/java/com/example/share_ops/FileChooser.java
+++ app/src/main/java/com/example/share_ops/FileChooser.java
@@ -58,11 +58,6 @@ public class FileChooser extends AppCompatActivity {
                 ArrayList<MediaFile> files = data.getParcelableArrayListExtra(FilePickerActivity.MEDIA_FILES);
                 Log.e("size","size is"+String.valueOf(files.size())+"path"+files.get(0).getPath());
 
-
-
-
-
-
                 String path = files.get(0).getPath();
                 filePath.setText(path);
                 Log.e("data", path);
@@ -77,9 +72,38 @@ public class FileChooser extends AppCompatActivity {
         if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
         } else {
-            Toast.makeText(this,"permission already granted",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "permission already granted", Toast.LENGTH_SHORT).show();
         }
 
+    }
+        public void chooseFile(View view){
 
+            Intent intent = new Intent(getApplicationContext(), FilePickerActivity.class);
+            intent.putExtra(FilePickerActivity.CONFIGS, new Configurations.Builder()
+                    .setCheckPermission(true)
+                    .setSelectedMediaFiles(files)
+                    .setShowFiles(true)
+                    .setShowImages(true)
+                    .setShowAudios(true)
+                    .setShowVideos(false)
+                    .setIgnoreNoMedia(false)
+                    .enableVideoCapture(true)
+                    .enableImageCapture(true)
+                    .setIgnoreHiddenFile(false)
+                    .setMaxSelection(1)
+                    .build());
+            startActivityForResult(intent, 1);
+        }
+
+        public void moveNext(View view){
+            System.out.println(new File(filePath.getText().toString()).exists());
+            if(new File(filePath.getText().toString()).exists()){
+                Intent intent = new Intent(this, MainActivity_Sender.class);
+                intent.putExtra("path",filePath.getText().toString());
+                intent.putParcelableArrayListExtra("list",files);
+                startActivity(intent);
+            } else {
+                Toast.makeText(this, "Please Select a File", Toast.LENGTH_SHORT).show();
+            }
     }
 }
