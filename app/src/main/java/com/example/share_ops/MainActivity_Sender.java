5fd2639ee0486315738f2df28c14cbce45dda259
Mokey-D-luffy88 <atching123@gmail.com>
2020-11-25 15:13:34 +0530

    if block incase it does not

--- app/src/main/java/com/example/share_ops/MainActivity_Sender.java
+++ app/src/main/java/com/example/share_ops/MainActivity_Sender.java
@@ -306,7 +306,16 @@ public class MainActivity_Sender extends AppCompatActivity{
                         String sAddr = addr.getHostAddress();
                         //boolean isIPv4 = InetAddressUtils.isIPv4Address(sAddr);
                         boolean isIPv4 = sAddr.indexOf(':')<0;
-
+                        if (useIPv4) {
+                            if (isIPv4)
+                                return sAddr;
+                        } else {
+                            if (!isIPv4) {
+                                int delim = sAddr.indexOf('%'); // drop ip6 zone suffix
+                                return delim<0 ? sAddr.toUpperCase() : sAddr.substring(0, delim).toUpperCase();
+                            }
+                        }
+                    }
 
         }
     }
