b2538e5ade70a52177992b42185bdaff8f0bb3c7
Mokey-D-luffy88 <atching123@gmail.com>
2020-11-25 15:12:32 +0530

    this give the list of ip's

--- app/src/main/java/com/example/share_ops/MainActivity_Sender.java
+++ app/src/main/java/com/example/share_ops/MainActivity_Sender.java
@@ -298,6 +298,14 @@ public class MainActivity_Sender extends AppCompatActivity{
                         }
     public static String getIPAddress(boolean useIPv4) {
         try {
+            List<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface intf : interfaces) {
+                List<InetAddress> addrs = Collections.list(intf.getInetAddresses());
+                for (InetAddress addr : addrs) {
+                    if (!addr.isLoopbackAddress()) {
+                        String sAddr = addr.getHostAddress();
+                        //boolean isIPv4 = InetAddressUtils.isIPv4Address(sAddr);
+                        boolean isIPv4 = sAddr.indexOf(':')<0;
 
 
         }
