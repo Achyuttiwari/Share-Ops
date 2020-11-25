ed88f7df888908d3b2d18d616ff1539f3c39634f
Mokey-D-luffy88 <atching123@gmail.com>
2020-11-25 11:09:09 +0530

    splash screen

--- app/src/main/java/com/example/share_ops/SplashScreen.java
+++ app/src/main/java/com/example/share_ops/SplashScreen.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SlashScreen extends AppCompatActivity {
+public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
